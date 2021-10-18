fa1f5d2b66738f16c98cffd58da5653c589dddf0
andrew-phelan <seridis@hotmail.com>
2021-10-18 14:31:22 +0100

    Create internal_nodes.py

---
+++ internal_nodes.py
@@ -0,0 +1,17 @@
+def find_internal_nodes_num(tree): 
+    root = -1
+    tree_details = {'all_nodes': [], 'parent_nodes': {}}
+    for leaf in tree:
+        if leaf == root:
+            continue
+        # Ensure we have all nodes from 0 to node
+        if len(tree_details.get('all_nodes')) < leaf:
+            tree_details['all_nodes'] = (list(range(-1, leaf + 2)))
+        if leaf not in tree_details.get('parent_nodes'):
+            tree_details['parent_nodes'][leaf] = 0
+        tree_details['parent_nodes'][leaf] += 1
+    return sum(tree_details['parent_nodes'].values())
+
+
+my_tree = [4, 4, 1, 5, -1, 4, 5]
+print(find_internal_nodes_num(my_tree))
